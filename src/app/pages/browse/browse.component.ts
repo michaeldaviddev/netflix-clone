18cdc688d89618283af240027feef0e004f7ba24
Michael David Machacuay <mdavidmachacuay@icloud.com>
2024-03-20 18:20:33 -0500

    create row component

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { RowComponent } from '../../components/row/row.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RowComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss'
 })
